7d71a269be2226d31e78b987b9acb8b986d17640
은선 남궁 <nammugmuhg@gmail.com>
2021-09-28 18:35:46 +0900

    페이지, Navbar 추가

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+function NavBar() {
+    const history = useHistory()
+    
+    return (
+        <div>
+            <header className="Basic-Bar">
+                <div >
+                    <div style={{ paddingLeft: "50px", fontSize: "30px" }} onClick={() => history.push("/")}>쓰랭</div></div>
+                <div style={{ display: "flex", justifyContent: "space-evenly"}}>
+                    <div onClick={() => history.push("/test")}>테스트하기</div>
+                    <div onClick={() => history.push("/ranking")}>랭킹보기</div>
+                </div>
+            </header>
+            <hr style={{
+                border : "solid 1px",
+                width: "95vw",
+                color: "rgb(80, 78, 78)"}} />
+        </div>
+    );
+}
+
+export default NavBar;
