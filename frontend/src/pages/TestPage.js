426e6453df09d9dbc29b22daff8558f02fb401af
heejae <jhj967878@naver.com>
2021-09-30 01:14:31 +0900

    # bug : axios 요청 버그 수정

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NavBar from '../components/NavBar';
 import axios from 'axios';
 import store from '../store';
 function TestPage() {
 
+    useEffect(() => {
+        axios({
+            method: "post",
+            url: "/api/test/test",
+            data: { name: store.getState().name, submit: [1, 2, 1, 2] }
+        })
+        .then((res)=>{console.log(res)})
+        .catch((e)=>alert(e));
+    })
+    
     return (
         <div >
             <NavBar />
-            {
-                axios.post({
-                    method: "post",
-                    url: "/api/test/test",
-                    data: { name: store.getState().name, submit: [1, 2, 1, 2] }
-                })
-                .then((res)=>{console.log(res)})
-                .catch((e)=>alert(e))
-            }
+            
         </div>
     )
 }
