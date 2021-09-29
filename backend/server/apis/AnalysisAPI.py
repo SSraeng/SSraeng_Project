b5e26aa61f0e49c5f98f9686de18cb2944a3786c
moonjiyoon <ske0830@gmail.com>
2021-09-29 19:20:54 +0900

    # feat : test 결과 주고받기

--- backend/server/apis/AnalysisAPI.py
+++ backend/server/apis/AnalysisAPI.py
@@ -1,2 +1,8 @@
+from flask.json import jsonify
+
 def analysis():
   return "분석 요청"
+
+
+def test_result(answer, submit):
+  return jsonify({"answer":answer, "submit":submit})
--- backend/server/controller/AnalysisController.py
+++ backend/server/controller/AnalysisController.py
@@ -1,9 +1,19 @@
-from flask import Blueprint
-from apis.AnalysisAPI import analysis
+from flask import Blueprint, request
+from apis.AnalysisAPI import analysis, test_result
 
 analysis_bp = Blueprint('analysis', __name__, url_prefix='/api')
 
 @analysis_bp.route('/analysis', methods=['GET'])
 def get_analysis_result():
   result = analysis()
+  return result
+
+
+# 테스트 결과 받고 전달
+@analysis_bp.route('/test', methods=['POST'])
+def test():
+  answer = request.json['answer']
+  submit = request.json['submit']
+
+  result = test_result(answer, submit)
   return result
