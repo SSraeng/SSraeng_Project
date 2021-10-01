e41072a0ce3bc85b2aed2b14446754de84c2e3c2
은선 남궁 <nammugmuhg@gmail.com>
2021-10-02 01:09:49 +0900

    #feat: Plastic계산기 기능 추가

--- frontend/src/components/PlasticCal.js
+++ frontend/src/components/PlasticCal.js
@@ -1,14 +1,21 @@
-import React,{useState} from 'react';
-
+import React,{useEffect, useState} from 'react';
+import PlusMinusButton from './PlusMinusButton';
+import plastic_store,{reset} from '../redux_store/plastic_store';
 function PlasticCal() {
-    const [times, setTimes] = useState(0)
+    const [sum, setSum] = useState(0)
+    const [result, setResult] = useState(0)
     return (
         <div>
-           <label>일주일 간 배달 건수</label>
-            <input value={times} type="number" onChange={(e) => setTimes(parseInt(e.target.value))} />
-            <button onClick={() => setTimes(parseInt(times + 1))}>+</button>
-            <button onClick={times > 1 ? () => setTimes(times - 1) : null}>-</button>
+            <div>플라스틱 계산기</div>
+            <PlusMinusButton title="비닐봉지" addNumber={10} />
+            <PlusMinusButton title="배달용기" addNumber={44} />
+            <PlusMinusButton title="일회용컵" addNumber={14} />
+            <PlusMinusButton title="PET" addNumber={15} />
+            <button onClick={() => { setResult(plastic_store.getState()[0]);}}>결과보기</button>
             <p></p>
+            당신이 일주일간 배달로 소비하는 플라스틱의 양은
+            <p></p>
+            {result}g입니다.
         </div>
     );
 }
