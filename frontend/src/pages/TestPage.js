8ffc0fca82e91cbacb3da54c952020537b434c9f
은선 남궁 <nammugmuhg@gmail.com>
2021-10-01 23:07:46 +0900

    #feat : 테스트 페이지 결과 store에 저장

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import NavBar from '../components/NavBar';
 import axios from 'axios';
-import store from '../store';
+import store,{submit} from '../store';
+import Tests from '../components/Tests'
+import { Link } from 'react-router-dom';
+
+
 function TestPage() {
+    const [num, setNum] = useState(0)
 
     return (
         <div >
-            <NavBar />
-            {
-                axios.post({
+            <NavBar/>
+            {/* {
+                
+                axios({
                     method: "post",
-                    url: "/api/test/test",
-                    data: { name: store.getState().name, submit: [1, 2, 1, 2] }
+                    url: "http://127.0.0.1:5000/api/test/test",
+                    data: store.getState()[0]
                 })
                 .then((res)=>{console.log(res)})
                 .catch((e)=>alert(e))
-            }
+            } */}
+            <Tests num={num} />
+            {num != 0?<button onClick={()=>setNum(num-1)}>이전</button>:null}
+            {num == 9 ? <Link to="/result"><button onClick={() => { store.dispatch(submit(store.getState()[0])); console.log(store.getState())}}>결과보기</button></Link>:<button onClick={()=>setNum(num+1)}>다음</button>}
         </div>
     )
 }
 
 export default TestPage;
+
