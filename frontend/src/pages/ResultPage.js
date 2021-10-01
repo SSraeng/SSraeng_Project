00d9fa16b0f01547359b9d3a3367bb5510cacac4
은선 남궁 <nammugmuhg@gmail.com>
2021-10-01 23:06:30 +0900

    #feat : 테스트 결과 페이지 점수와 등급 나타내기

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import store from '../store';
+import { first,second,third,fourth,fifth } from '../tests/level';
 function ResultPage() {
-
+    const result = []
+    let index = 0
+    const answer = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    let src = ""
+    for (let i of store.getState()[0].submit) {
+        if (answer[index] == i)
+            result.push(1)
+        else
+            result.push(0)
+        index += 1
+    }
+    const score = result.reduce(function add(a, b) { return a + b; }, 0) * 10
+    src = score < 50 ? fifth : score < 70 ? fourth : score < 90 ? third : score < 100 ? second : first
     return (
         <div >
             <NavBar />
-            {store.getState().name}
+            <p>나의  점수</p>
+            <div>{score}</div>
+            <img src={src}></img>
         </div>
     )
 }
