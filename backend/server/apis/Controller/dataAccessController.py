d6e4df58d7b6c670cc26057ca5beefe95fc4f5c3
heejae <jhj967878@naver.com>
2021-10-04 21:43:13 +0900

    # chore : 임시 데이터 파일 삭제

--- backend/server/apis/Controller/dataAccessController.py
+++ backend/server/apis/Controller/dataAccessController.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request
 
-from apis.dto.responseDto import temp_data_response_dto, user_response_dto
-from service.Ranking import get_rank
+from apis.dto.responseDto import user_response_dto
 from service.Users import get_user
 
 data_api_bp = Blueprint('data_api', __name__, url_prefix='/api')
@@ -10,8 +9,3 @@ data_api_bp = Blueprint('data_api', __name__, url_prefix='/api')
 def get_users():
   users = get_user()
   return user_response_dto(users)
-
-@data_api_bp.route('/ranking', methods=['GET'])
-def get_ranking():
-  rankings = get_rank()
-  return temp_data_response_dto(rankings)
