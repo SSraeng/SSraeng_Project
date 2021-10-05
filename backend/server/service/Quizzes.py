7e760fc3de9f9d55675990c93f200ce37583b3c7
moonjiyoon <ske0830@gmail.com>
2021-10-05 11:52:08 +0900

    # feat: quiz, answer 요청 api

--- backend/server/service/Quizzes.py
+++ backend/server/service/Quizzes.py
@@ -3,13 +3,17 @@ from domain.dao.answerDao import all_answers
 
 def get_quiz():
   quiz_data = all_quiz()
-  result = []
+  result = {}
   for quiz in quiz_data:
     quiz_number = "Q" + str(quiz.id)
-    answers = all_answers(quiz.id)
-    result.append({
-      quiz_number:{
+
+    answer_list = all_answers(quiz.id)
+    answers = []
+    for answer in answer_list:
+      answers.append(answer.content)
+
+    result[quiz_number] = {
         "question":quiz.question,
         "answers":answers
-    }})
+    }
   return result
