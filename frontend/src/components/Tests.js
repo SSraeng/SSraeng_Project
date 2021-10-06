96310f44bb0eaf297c497cb3c94db0bf291d7d0d
남궁은선 <nammugmuhg@gmail.com>
2021-10-06 19:41:18 +0900

    #feat : 선택지 누르면 색변함

--- frontend/src/components/Tests.js
+++ frontend/src/components/Tests.js
@@ -1,20 +1,29 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import test from "../tests/test.json"
 import store, { add } from '../redux_store/store';
 import { Hover } from '../styled_components/hover';
+import { element } from 'prop-types';
 
 function Tests({num}) {
     const [active,setActive] = useState(false)
     const questions = Object.values(test[Object.keys(test)[num-1]])[0]
     const answers = Object.values(test[Object.keys(test)[num-1]])[1]
+    const[isClicked,setIsClicked] = useState(Array(4).fill(false))
+    useEffect(()=>{
+        setIsClicked(Array(4).fill(false))
+    },[num])
     return (
         
         <div>
             <Hover>
             {questions}
             <p></p>
-            {answers.map((answer, index)=>
-                <div key={index} onClick={() => { store.dispatch(add({index:num-1,submit:index+1}));console.log(store.getState().test[0].submit);}}>{`${index+1}.`}{answer}</div>)}
+            {answers.map((answer, i)=>
+                <div key={i} style={{backgroundColor: isClicked[i] ? 'blue' : 'red'}} 
+                onClick={(e) => { 
+                    store.dispatch(add({index:num-1,submit:i+1}));
+                    setIsClicked(isClicked.map((element,index)=>{return index==i? element=true:element=false}));
+                }}>{`${i+1}.`}{answer}</div>)}
             </Hover>        
         </div>
         
