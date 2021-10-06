fbd506b7b7aa6c61ad8d4c3ee222c2e74f4908df
heejae <jhj967878@naver.com>
2021-10-06 22:40:59 +0900

    # refactor : 불필요한 파일 삭제

--- backend/server/apis/dto/responseDto.py
+++ backend/server/apis/dto/responseDto.py
@@ -1,13 +1,5 @@
 from flask.json import jsonify
 
-# 데이터를 받아 JSON 포맷으로 Response를 리턴 해주는 함수
-def temp_data_response_dto(temp_data):
-  response = []
-  for data in temp_data:
-    response.append({'id':data['id'], 'name':data['name']})
-    
-  return jsonify(response)
-
 def user_response_dto(user):
   return jsonify(user),200
 
