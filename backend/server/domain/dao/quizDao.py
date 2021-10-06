403ae44498ab77c8a828b71be56053e4ca5be4bb
[2기-1반] 정희재 <jhj967878@naver.com>
2021-10-06 15:17:50 +0000

    Revert "Merge branch 'Back/model' into 'master'"
    This reverts merge request !17

--- backend/server/domain/dao/quizDao.py
+++
@@ -1,5 +0,0 @@
-from domain.models.quiz import Quiz
-
-def all_quiz():
-  result = Quiz.query.all()
-  return result
--- backend/server/domain/dao/userDao.py
+++ backend/server/domain/dao/userDao.py
@@ -1,11 +1,5 @@
-from domain.models.user import User, db
+from domain.models.user import User
 
 def all_user():
-  result = User.query.order_by(User.id.desc()).all()
+  result = User.query.all()
   return result
-
-def new_user(name):
-  user = User(name)
-  db.session.add(user)
-  db.session.commit()
-  return user
