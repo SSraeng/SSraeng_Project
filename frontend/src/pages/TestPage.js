3b0f7803d44052da292f8928597233c85bb79ed3
남궁은선 <nammugmuhg@gmail.com>
2021-10-06 15:57:27 +0900

    #feat:progress bar 테스트페이지에 추가

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -5,13 +5,15 @@ import store, { submit, reset , plastic_reset} from '../redux_store/store';
 import Tests from '../components/Tests'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import ProgressBar from '../components/ProgressBar';
 function TestPage() {
     const [num, setNum] = useState(1)
     const useSubmit = useSelector(state=>state.test[0].submit)
     return (
         <div >
             <NavBar/>
+            <ProgressBar num={num}/>
+
             <Tests num={num} />
             {num != 1?<button onClick={()=>{setNum(num-1); console.log(useSubmit.length);}}>이전</button>:null}
             {num == 10 ? 
