015f97631a97ca92537d953ea0f7e1c533622af5
남궁은선 <nammugmuhg@gmail.com>
2021-10-06 11:17:31 +0900

    #feat: 테스트 다음버튼 활성화 기능추가.

--- frontend/src/components/Tests.js
+++ frontend/src/components/Tests.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import test from "../tests/test.json"
 import store, { add } from '../redux_store/store';
+import { Hover } from '../styled_components/hover';
 
 function Tests({num}) {
-
-    const questions = Object.values(test[Object.keys(test)[num]])[0]
-    const answers = Object.values(test[Object.keys(test)[num]])[1]
+    const [active,setActive] = useState(false)
+    const questions = Object.values(test[Object.keys(test)[num-1]])[0]
+    const answers = Object.values(test[Object.keys(test)[num-1]])[1]
     return (
+        
         <div>
+            <Hover>
             {questions}
             <p></p>
             {answers.map((answer, index)=>
-                <div key={index} onClick={() => { store.dispatch(add(index + 1)); console.log(store.getState().test[0].submit)}}>{`${index+1}.`}{answer}</div>)}
+                <div key={index} onClick={() => { store.dispatch(add({index:num-1,submit:index+1}));console.log(store.getState().test[0].submit);}}>{`${index+1}.`}{answer}</div>)}
+            </Hover>        
         </div>
+        
     );
 }
 
--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -4,18 +4,29 @@ import axios from 'axios';
 import store, { submit, reset , plastic_reset} from '../redux_store/store';
 import Tests from '../components/Tests'
 import { Link } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 
 function TestPage() {
-    const [num, setNum] = useState(0)
+    const [num, setNum] = useState(1)
+    const useSubmit = useSelector(state=>state.test[0].submit)
     return (
         <div >
             <NavBar/>
             <Tests num={num} />
-            {num != 0?<button onClick={()=>setNum(num-1)}>이전</button>:null}
-            {num == 9 ? <Link to="/result"><button onClick={() => {
-                store.dispatch(submit(store.getState()[0]));
-            }}>결과보기</button></Link> : <button onClick={() => setNum(num + 1)}>다음</button>}
+            {num != 1?<button onClick={()=>{setNum(num-1); console.log(useSubmit.length);}}>이전</button>:null}
+            {num == 10 ? 
+            <Link to="/result">
+                <button onClick={
+                    () => {
+                store.dispatch(submit(store.getState().test[0]));
+                        }
+            }>
+                결과보기
+                </button>
+            </Link>
+             : <button onClick={() =>{setNum(num+1); console.log(useSubmit.length);}} disabled={
+                useSubmit.length>=num ? false:true
+            }>다음</button>}
         </div>
     )
 }
