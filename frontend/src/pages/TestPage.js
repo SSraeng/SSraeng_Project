cfaa414f31c0ea5e46d757ba34ce12a04785e6b7
남궁은선 <nammugmuhg@gmail.com>
2021-10-06 22:14:43 +0900

    #chore:테스트페이지에서 결과페이지로 넘어간 후 뒤로가기 해도 테스트 페이지 나오지 않도록 history.replace  사용

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -1,34 +1,35 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import NavBar from '../components/NavBar';
 import axios from 'axios';
-import store, { submit, reset , plastic_reset} from '../redux_store/store';
+import store, { submit, reset, plastic_reset } from '../redux_store/store';
 import Tests from '../components/Tests'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProgressBar from '../components/ProgressBar';
+import { useHistory } from 'react-router';
 function TestPage() {
     const [num, setNum] = useState(1)
-    const useSubmit = useSelector(state=>state.test[0].submit)
+    const useSubmit = useSelector(state => state.test[0].submit)
+    const history = useHistory()
     return (
         <div >
-            <NavBar/>
-            <ProgressBar num={num}/>
+            <NavBar />
+            <ProgressBar num={num} />
 
             <Tests num={num} />
-            {num != 1?<button onClick={()=>{setNum(num-1); console.log(useSubmit.length);}}>이전</button>:null}
-            {num == 10 ? 
-            <Link to="/result">
+            {num != 1 ? <button onClick={() => { setNum(num - 1); console.log(useSubmit.length); }}>이전</button> : null}
+            {num == 10 ?
                 <button onClick={
                     () => {
-                store.dispatch(submit(store.getState().test[0]));
-                        }
-            }>
-                결과보기
+                        store.dispatch(submit(store.getState().test[0]));
+                        history.replace("/result")
+                    }
+                }>
+                    결과보기
                 </button>
-            </Link>
-             : <button onClick={() =>{setNum(num+1); console.log(useSubmit.length);}} disabled={
-                useSubmit.length>=num ? false:true
-            }>다음</button>}
+                : <button onClick={() => { setNum(num + 1);}} disabled={
+                    useSubmit.length >= num ? false : true
+                }>다음</button>}
         </div>
     )
 }
