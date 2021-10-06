f426c31dbe8b3cec05b932cb8cd990dc0dd78fc9
남궁은선 <nammugmuhg@gmail.com>
2021-10-06 22:13:19 +0900

    #feat:Test컴포넌트에서 다음버튼 누르고 이전에 가도 선택표시가 남아있도록 하는 기능

--- frontend/src/components/Tests.js
+++ frontend/src/components/Tests.js
@@ -1,32 +1,30 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import test from "../tests/test.json"
 import store, { add } from '../redux_store/store';
 import { Hover } from '../styled_components/hover';
-import { element } from 'prop-types';
 
-function Tests({num}) {
-    const [active,setActive] = useState(false)
-    const questions = Object.values(test[Object.keys(test)[num-1]])[0]
-    const answers = Object.values(test[Object.keys(test)[num-1]])[1]
-    const[isClicked,setIsClicked] = useState(Array(4).fill(false))
-    useEffect(()=>{
-        setIsClicked(Array(4).fill(false))
-    },[num])
+function Tests({ num }) {
+    const questions = Object.values(test[Object.keys(test)[num - 1]])[0]
+    const answers = Object.values(test[Object.keys(test)[num - 1]])[1]
+    const [clickedNum, setClickedNum] = useState(Array(10).fill(0))
+
     return (
-        
+
         <div>
             <Hover>
-            {questions}
-            <p></p>
-            {answers.map((answer, i)=>
-                <div key={i} style={{backgroundColor: isClicked[i] ? 'blue' : 'red'}} 
-                onClick={(e) => { 
-                    store.dispatch(add({index:num-1,submit:i+1}));
-                    setIsClicked(isClicked.map((element,index)=>{return index==i? element=true:element=false}));
-                }}>{`${i+1}.`}{answer}</div>)}
-            </Hover>        
-        </div>
+                {questions}
+                <p></p>
+                {answers.map((answer, i) =>
         
+                    <div key={i} style={{ backgroundColor: clickedNum[num-1] == i+1 ? 'blue' : 'red' }}
+                        onClick={(e) => {
+                            store.dispatch(add({ index: num - 1, submit: i + 1 }));
+                            setClickedNum(clickedNum.map((element, index) => {return index+1 == num ? element = i+1 : element}));
+
+                        }}>{`${i + 1}.`}{answer}</div>)}
+            </Hover>
+        </div>
+
     );
 }
 
