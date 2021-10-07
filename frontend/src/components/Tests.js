770f1c101ed7ed907c11e947d68b8a114e43c9b0
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-08 03:12:27 +0900

    #chore: test 컴포넌트에 hover 컴포넌트 추가 및 기타 수정

--- frontend/src/components/Tests.js
+++ frontend/src/components/Tests.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import test from "../tests/test.json"
 import store, { add } from '../redux_store/store';
-// import { Hover } from '../styled_components/Hover';
 import axios from 'axios';
+import { Hover } from '../styled_components/Hover';
 function Tests({ num }) {
    
         const [test,setTest] = useState([]);
@@ -13,7 +13,6 @@ function Tests({ num }) {
                     setTest([]);
                     const  response = await axios.get("/api/test/test");
                     setTest(response.data);
-                    console.log(response.data[0].question);
                 }catch(e){
                     console.log(e)
                 }
@@ -31,16 +30,16 @@ function Tests({ num }) {
              {
              test.length === 0 ? <div> Loading... </div> :
             <div>
-                 {test[num-1].question}
-                <p></p>
+                 <div>Q{num}.{test[num-1].question}</div>
                 {test[num-1].examples.map((answer, i) =>
-                    
+                    <Hover>
                     <div key={i} style={{ backgroundColor: clickedNum[num-1] == i+1 ? 'blue' : 'red' }}
                         onClick={(e) => {
                             store.dispatch(add({ index: num - 1, submit: i + 1 }));
                             setClickedNum(clickedNum.map((element, index) => {return index+1 == num ? element = i+1 : element}));
 
                         }}>{`${i + 1}.`}{answer}</div>
+                        </Hover>
                         )}
             </div>
             }
