20c14f39fcaa66f235c3f1b906596cc10365eb23
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-07 18:54:57 +0900

    #feat:백엔드에서 결과 불러오기 . 아직 주석처리

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import NavBar from '../components/NavBar';
-import store,{reset} from '../redux_store/store';
+import store,{reset,submit} from '../redux_store/store';
 import { first, second, third, fourth, fifth } from '../tests/level';
 import PlasticCal from '../components/PlasticCal';
+import axios from 'axios';
 
 function ResultPage() {
+    // const [score,setScore]= useState(null)
+    // const [src, setSrc]= useState(null)
+    // useEffect(()=>{
+    //     const testResult = async()=>{
+    //         try{
+    //             setScore(null);
+    //             setSrc(null);
+    //             const userId = await axios.post("/api/analysis",store.getState().test[0]);
+    //             const response= await axios.get(`/api/result?user_id=${response.data.user_id}`)
+    //             setScore(response.data.score)
+    //             setSrc(response.data.tier)
+    //         }catch(e){
+    //             console.log(e)
+    //         }
+    //     }
+        
+
+    //     testResult();
+    // },[])
+
     const result = []
     let index = 0
     const answer = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     let src = ""
-    for (let i of store.getState().test[0].submit) {
+    for (let i of store.getState().test[0].answers) {
         if (answer[index] == i)
             result.push(1)
         else
@@ -18,6 +39,7 @@ function ResultPage() {
     }
     const score = result.reduce(function add(a, b) { return a + b; }, 0) * 10
     src = score < 50 ? fifth : score < 70 ? fourth : score < 90 ? third : score < 100 ? second : first
+
     return (
         <div >
             <NavBar />
