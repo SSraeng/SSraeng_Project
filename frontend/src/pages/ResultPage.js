edec430f12714ccd2a8268a797dbbff0c69fee5c
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-08 20:10:56 +0900

    #feat: 결과 불러오기

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -6,18 +6,17 @@ import PlasticCal from '../components/PlasticCal';
 import axios from 'axios';
 
 function ResultPage() {
-    const [score,setScore]= useState(null)
-    const [src, setSrc]= useState(null)
+    const [data,setData]= useState(null)
     useEffect(()=>{
         const testResult = async()=>{
             try{
-                setScore(null);
-                setSrc(null);
+                setData(null);
+                console.log(store.getState().test[0])
                 const userId = await axios.post("/api/analysis",store.getState().test[0]);
                 console.log(userId)
-                // const response= await axios.get(`/api/result?user_id=${response.data.user_id}`)
-                // setScore(response.data.score)
-                // setSrc(response.data.tier)
+                const response= await axios.get(`/api/result/${userId.data}`)
+                console.log(response.data);
+                setData(response.data)
             }catch(e){
                 console.log(e)
             }
@@ -27,30 +26,27 @@ function ResultPage() {
         testResult();
     },[])
 
-    // const result = []
-    // let index = 0
-    // const answer = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    // let src = ""
-    // for (let i of store.getState().test[0].answers) {
-    //     if (answer[index] == i)
-    //         result.push(1)
-    //     else
-    //         result.push(0)
-    //     index += 1
-    // }
-    // const score = result.reduce(function add(a, b) { return a + b; }, 0) * 10
-    // src = score < 50 ? fifth : score < 70 ? fourth : score < 90 ? third : score < 100 ? second : first
-
     return (
         <div >
             <NavBar />
-            <div style={{margin:"auto", textAlign:"center", marginTop:"10vh" }}>
-            <h1>나의  점수</h1>
-            <h2>{score}</h2>
-            <img src={src} width="300px"style={{marginBottom:"5vh"}}></img>
-            
-            <PlasticCal/>
+            {
+            data?
+            <div style={{position: "absolute",left: "50%",transform: "translate(-50%)"}}>
+                <div style={{margin:"auto", textAlign:"center", marginTop:"10vh" }}/>
+                <h1>{data.user_name}의  점수</h1>
+                <h2>{data.score}</h2>
+                <img src={data.tier} width="300px"style={{marginBottom:"5vh"}}/>
+                <img src={data.recycle_tip} width="300px"style={{marginBottom:"5vh"}}/>
+                <div>{data.content_text}</div>
+                <a href={data.content_url}><img src={data.content_image}/></a>
+                
+                <PlasticCal/>
             </div>
+
+            :null
+            }
+
+            
         </div>
     )
 }
