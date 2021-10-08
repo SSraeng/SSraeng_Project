faa726a6157c5fc3ee0015a4d87a9cff8155d03d
heejae <jhj967878@naver.com>
2021-10-08 22:59:26 +0900

    # feat : Ranking API 데이터 포맷 수정

--- backend/server/service/rankService.py
+++ backend/server/service/rankService.py
@@ -9,7 +9,10 @@ def get_user_ranking():
   rank_info = {90: 5, 80: 4, 50:3, 20:2}
   
   for user, result in zip(user_data, result_data):
-    final_score = result.quiz_score*10 + (1/result.exam_time) - 3*result.delivery_count*(result.delivery_count+1)/2
+    quiz_score = result.quiz_score*10
+    delivery_count = result.delivery_count
+    final_score = quiz_score + (1/result.exam_time) - 3*delivery_count*(delivery_count+1)/2
+    total_score = quiz_score - 3*delivery_count*(delivery_count+1)/2
     
     tier = 0
     for key, value in rank_info.items():
@@ -19,7 +22,7 @@ def get_user_ranking():
     if tier == 0:
       tier = 1
       
-    ranking.append({'name':user.name, 'score':final_score, 'tier': tier})
+    ranking.append({'name':user.name, 'final_score':final_score, 'tier': tier, 'quiz_score':quiz_score, 'delivery_count': delivery_count, 'total_score': total_score})
     
-  ranking.sort(key = lambda x: x['score'])
+  ranking.sort(key = lambda x: x['final_score'])
   return ranking
