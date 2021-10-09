30ea383115a624c1c5588e82eb049784eeaafb7f
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-10 03:56:41 +0900

    #feat: userInfo페이지 렌더링시 스토어 리셋

--- frontend/src/pages/UserInfoPage.js
+++ frontend/src/pages/UserInfoPage.js
@@ -9,8 +9,14 @@ import { Hover } from '../styled_components/Hover';
 function UserInfoPage() {   
     const history = useHistory()
     const [nickname, setNickname] = useState("")
-    const [times, setTimes] = useState("");
-    return (
+    const [times, setTimes] = useState(0);
+
+    useEffect(()=>{
+        store.dispatch(reset())
+        store.dispatch(plastic_reset())
+        store.dispatch(page_reset())
+    },[])
+        return (
         <div>
             <NavBar/>
         <div style={{marginLeft:"auto",textAlign:"center"}}>
@@ -20,8 +26,11 @@ function UserInfoPage() {
             <h2 style={{marginBottom:"1vh"}}>이름</h2>
             <input value={nickname} style={{textAlign:"center", marginBottom:"3vh" }}onChange={(e) => setNickname(e.target.value)} />
             <h2 style={{marginBottom:"1vh"}}>일주일 간 배달 건수</h2>
-            <button style={{borderRadius:"1vh",marginRight:"1vh",border : "0",marginBottom:"6vh", width:"4vh", backgroundColor:"#2aa6dc"}} onClick={times > 0 ? () => setTimes(times - 1) : null}>-</button>
-            <input value={times}  style={{textAlign:"center"}} onChange={(e) => setTimes(parseInt(e.target.value))} readOnly/>
+            <button style={{borderRadius:"1vh",marginRight:"1vh",border : "0",marginBottom:"6vh", width:"4vh", backgroundColor:"#2aa6dc"}} 
+            onClick={times > 0 ? () => setTimes(times - 1) : null}>-</button>
+
+            <input value={times} style={{textAlign:"center"}} onChange={(e) => setTimes(parseInt(e.target.value))} readOnly/>
+
             <button style={{borderRadius:"1vh",marginLeft:"1vh",border : "0",marginBottom:"6vh", width:"4vh", backgroundColor:"#2aa6dc"}} onClick={() => setTimes(parseInt(times + 1))}>+</button>
             <p></p>
             <Hover>
@@ -36,7 +45,7 @@ function UserInfoPage() {
                     store.dispatch(user({ name: nickname, times: times }));
                     history.push("/test/test")
             }
-            } disabled={!(times&&nickname)}><h1>테스트 시작</h1></button>
+            } disabled={!nickname}><h1>테스트 시작</h1></button>
             </Hover>
             </div>
 
