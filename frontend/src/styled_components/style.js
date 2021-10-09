2ea7306da3eb3a3b6d47eb034cb5d3a5afdff5dd
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-10 03:59:28 +0900

    #chore: hover.js를 style.js로 바꾸기

---
+++ frontend/src/styled_components/style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+export const Hover = styled.div`
+
+    &:hover{
+        cursor:pointer;
+        transform:scale(1.2);
+
+    }
+
+`
+
+export const Select = styled(Hover)`
+    width:35vh;
+    height:5vh; 
+    background-color: ${(props)=>props.num == props.i+1 ? "#2aa6dc" : "#e7feff"};
+    margin: 0 auto; 
+    margin-top:1vh;
+    border-radius: 2vh;
+    display: flex;
+    flex-direction:column;
+    justify-content:center;
+    }
+    &:hover{
+        background-color: #87F5F5
+
+    }
+
+`
