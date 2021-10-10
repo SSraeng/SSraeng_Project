8cb1c5b3a507364d696a9afb9ec99869974777a9
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-11 02:23:40 +0900

    #feat: 결과페이지 페이지네이션

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -1,15 +1,16 @@
 import React, { useState,useEffect } from 'react';
 import NavBar from '../components/NavBar';
 import store,{reset,submit} from '../redux_store/store';
-import { first, second, third, fourth, fifth } from '../tests/level';
 import PlasticCal from '../components/PlasticCal';
 import axios from 'axios';
+import { useHistory } from 'react-router';
+
 
 function ResultPage() {
 
     const [page, setPage] = useState(1);
-
-    const data= store.getState().result
+    const history = useHistory()
+    const data= store.getState().result[0]
     const {user_name,
         score,
         tier,
@@ -17,26 +18,68 @@ function ResultPage() {
         content_text,
         content_url,
         content_image
-     } = store.getState().result[0]
-    console.log(data);
+     } = data
+     const datalist = [
+         user_name,
+        score,
+        tier,
+        recycle_tip,
+        content_text,
+        content_url,
+        content_image
+    ]
     return (
         <div >
             <NavBar />
+            <div align="center" ><img src="images/result_polarbear.jpg" style={{width:"80vh", 
+        }} ></img></div>
             {
             data?
             <div style={{position: "absolute",left: "50%",transform: "translate(-50%)", textAlign:"center"}}>
                 <div style={{margin:"auto", textAlign:"center", marginTop:"10vh" }}/>
-                <h1 style={{textAlign:"center"}}>{user_name}의  점수</h1>
-                <h2 style={{textAlign:"center"}}>{score}</h2>
-                <div>
-                <img src={tier} width="300px"style={{marginBottom:"5vh" }} /></div>
-                <div>
-                <img src={recycle_tip} width="300px"style={{marginBottom:"5vh"}}/></div>
                 
-                <div>{content_text}</div>
-                <a href={content_url}><img width = "300vh" src={content_image}/></a>
+                <div style={{display:'flex', alignContent:"center"}}>
+                    {page !=1?
+                        <div style={{margin:"auto", paddingRight:"2vh"}} onClick={()=>setPage(page-1)}>
+                            〈
+                        </div>:
+                        null
+                    }
+                    {page == 1?
+                    
+                    <div style={{marginLeft:"2vh", marginTop:"2vh"}}>
+                        <h1 style={{textAlign:"center"}}>{user_name}의  점수</h1>
+                        <h2 style={{textAlign:"center"}}>{score}</h2>
+                        <div>
+                            <img src={tier} width="300px"style={{marginTop:"2vh",marginBottom:"5vh", display:"block" }} />
+                        </div>
+                        
+                    </div>:
+                    page == 2?
+                    <div>
+                    <img src={recycle_tip} width="300px"style={{marginBottom:"5vh"}}/>
+                    </div>:
+                    page ==3?
+                    <div>
+                        <div>{content_text}</div>
+                        <a href={content_url}>
+                            <img width = "300vh" src={content_image}/>
+                        </a>
+                    </div>:
+                    page == 4?
+                    <PlasticCal/>:
+                    null
+                    }
+                    {page !=4?
+                    <div style={{margin:"auto", paddingLeft:"2vh"}} onClick={()=>setPage(page+1)}>
+                        〉
+                    </div>:
+                    null
+                    }
+                </div>
                 
-                <PlasticCal/>
+                
+                <button onClick={()=>history.replace("/ranking")}>나의 랭킹 확인하기</button>
             </div>
 
             :null
