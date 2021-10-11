9105b0d9d795c61a50e9b6671688d33a490bb6ab
heejae <jhj967878@naver.com>
2021-10-11 19:32:59 +0900

    # chore : 칼럼 타입 String -> NVARCHAR 변경

--- backend/server/domain/models/result.py
+++ backend/server/domain/models/result.py
@@ -7,11 +7,13 @@ class Result(db.Model):
   id             = db.Column(db.Integer, primary_key=True, nullable=False)
   user_id        = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
   quiz_score     = db.Column(db.Integer, nullable=False)
+  ox_list        = db.Column(db.String(256), nullable=False, server_default='OOOXXXOOXX')
   exam_time      = db.Column(db.Integer, nullable=False)
   delivery_count = db.Column(db.Integer, nullable=False)
   
-  def __init__(self, user_id, quiz_score, exam_time, delivery_count):
+  def __init__(self, user_id, quiz_score, ox_list, exam_time, delivery_count):
     self.user_id        = user_id
     self.quiz_score     = quiz_score
+    self.ox_list        = ox_list
     self.exam_time      = exam_time
     self.delivery_count = delivery_count
