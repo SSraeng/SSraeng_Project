f963c632459f4a098e3dcb3ff61dce947b9b050d
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-12 02:38:08 +0900

    #feat: 랭킹페이지 user_id 유무에 따라 다르게 나타내고, user_id있을 땐 user의 랭킹이 있는 페이지부터 보이게끔 설정

--- frontend/src/pages/RankingPage.js
+++ frontend/src/pages/RankingPage.js
@@ -2,20 +2,54 @@ import React, { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
 import axios from 'axios';
 import { RankElement } from '../styled_components/style';
-function RankingPage() {
-    const [ranks, setRanks] = useState(null)
-    const [start,setStart]= useState(1);
-    const getRanking = async () =>{
-        const response= await axios.get("/api/ranking")
+import store from '../redux_store/store';
 
-        const{data} = response
-        setRanks(data);
-        console.log(data)
+function RankingPage({match}) {
+    const {params} = match
+    const [rankData, setRankData] = useState(null)
+    const [userRank,setUserRank] = useState(null)
+    const user_id = store.getState().user_id[0]
+    const [start,setStart]= useState(0);
+
+
+    const getRanking = async () =>{
+        if (user_id)
+        {
+            const response= await axios.get(`/api/ranking/${user_id}`)
+            const{ranks} = response.data
+            const{user} = response.data
+            const {ranking} = user
+            setRankData(ranks);
+            setUserRank(user);
+            setStart(15*Math.floor(ranking/15))
+            console.log(ranks)
+
+        }
+        else{ const response= await axios.get("/api/ranking") 
+        const {ranks} = response.data
+        setRankData(ranks);
+        console.log(response)
     }
+
+    }
+    useEffect(()=>{
+        getRanking();
+    },[])
     return (
         <div >
             <NavBar />
-
+            {
+                userRank?
+                <div style={{display:"flex", justifyContent:"space-evenly"}}>  
+                <RankElement width={1}>{userRank.ranking}</RankElement>
+                <RankElement width={10}>{userRank.name}</RankElement>
+                <RankElement width={2}>{userRank.tier}</RankElement>
+                <RankElement width={4}>{userRank.delivery_count}</RankElement>
+                <RankElement width={5}>{userRank.quiz_score}</RankElement>
+                <RankElement width={4}>{userRank.total_score}</RankElement> 
+            </div>:null
+            }
+            
             <button onClick={getRanking}>랭킹불러오기</button>
             <div style={{display:"flex", justifyContent:"space-evenly"}}>    
                 <RankElement width={1}></RankElement>
@@ -26,10 +60,11 @@ function RankingPage() {
                 <RankElement width={4}>총 점수</RankElement> 
             </div>
             <div >
-            {ranks?
-            ranks.slice(start,start+15).map((element,index)=>
-            <div key={index} style={{display:"flex", justifyContent:"space-evenly"}} >
-            <RankElement width={1}>{index+start}</RankElement>
+            {rankData?
+            rankData.slice(start,start+15).map((element,index)=>
+            <div key={index+start} style={{display:"flex", justifyContent:"space-evenly", 
+            border:`${userRank && index+start+1 === userRank.ranking? "solid 4px":null}`}} >
+            <RankElement width={1}>{index+start+1}</RankElement>
             <RankElement width={10}>{element.name}</RankElement>
             <RankElement width={2}>{element.tier}</RankElement>
             <RankElement width={4}>{element.delivery_count}</RankElement>
@@ -40,8 +75,10 @@ function RankingPage() {
             </div>
             {start > 14? <button onClick={()=>{setStart(start-15)}}>이전으로</button>: null}
             {
-            !ranks? null:
-            start < ranks.length?<button onClick={()=>{setStart(start+15)}}>다음으로</button>:null
+            !rankData? null:
+            start > rankData.length - 15?
+            <button onClick={()=>{setStart(0)}}>처음으로</button>
+            :<button onClick={()=>{setStart(start+15)}}>다음으로</button>
             }
         </div>
     )
