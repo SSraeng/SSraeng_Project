7482844819358290cebb5fc24547216ab6dc115a
heejae <jhj967878@naver.com>
2021-10-13 01:59:47 +0900

    # feat : WhichiAction 컴포넌트

---
+++ frontend/src/styled_components/WhichActionStyle.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const WhichActionWrapper = styled.div`
+    text-align: center;
+    margin-top: 5vh;
+`;
+
+export const WhichActionExample = styled.div`
+  width: 50vw;
+  margin: 0 auto;
+  /* display: grid; */
+  /* grid-template-columns: 20vw; */
+  div{
+    margin-top: 1vh;
+    margin-bottom: 1vh;
+    font-size: 1.7rem;
+
+    span{
+      color: rgb(0,150,0);
+    }
+  }
+
+  button{
+    text-align: center;
+    display: inline-block;
+    width: 35vw;
+    margin-bottom: 3vh;
+    background-color: rgba(0,255,0,0.2);
+    border: none;
+    border-radius: 15px;
+    font-size: 1.4rem;
+    min-height: 2.5em;
+
+    :hover{
+      cursor: pointer;
+      font-weight: bold;
+    }
+  }
+`;
+
+export const WhichActionButtonWrapper = styled.div`
+  padding-top: 3vh;
+`;
