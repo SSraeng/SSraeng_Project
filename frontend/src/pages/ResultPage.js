fff198f485a13344bf119485eea0aaea275d7465
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-13 02:03:06 +0900

    #feat: resultpage 컴포넌트 불러오고 배치하기

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -1,27 +1,20 @@
 import React, { useState,useEffect } from 'react';
 import NavBar from '../components/NavBar';
-import store from '../redux_store/store';
+import store, { plastic_reset, reset_co2 } from '../redux_store/store';
 import PlasticCal from '../components/PlasticCal';
 import axios from 'axios';
 import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
-import Grade from '../resultpage-components/Grade';
-import WhichAction from '../resultpage-components/WhichAction';
-
+import {Grade,PlasticResult,PolarBearTV,PolarBearTVMent,RecycleTip,Solutions,WhichAction} from "../resultpage-components/components"
 function ResultPage({match}) {
     const {params} = match
     const history = useHistory()
     const [resultData,setResultData] = useState(false);
 
     const user_id = store.getState().user_id[0]
-    const result= store.getState().result[0]
-    if (user_id)
-    {
-
-        console.log(result)
-
-    }
-
+    const result = store.getState().result[0]
+    const {ox_list, user_name, content_url, content_image}= result
+    const [page,setPage]= useState(1)
      useEffect(()=>{
             const getResult = async () =>{
 
@@ -35,11 +28,38 @@ function ResultPage({match}) {
 
     return (
         <div>
-            <NavBar />
-           { resultData?<Grade data={resultData}/>:result?<Grade data={result}/>:null}
-            {result?<WhichAction user_id={user_id} user_name={result.user_name} history={history}/>:null}
+            <NavBar/>
+           {page==1?<Grade data={resultData?resultData:result?result:null}/>:null}
+           {ox_list&&page==2?
+            <Solutions oxlist={ox_list}/>
+            :user_id&&page==3?
+            <WhichAction user_id={user_id} user_name={user_name} history={history}/>
+            
+            :result&&page==4?
+            <PolarBearTV content_url={content_url} content_image={content_image}/>
+            :user_name&&page==5?
+            <RecycleTip/>
+            :user_name&&page==6?
+            <PolarBearTVMent/>
+            :user_name&&page==7?
+            
+            <PlasticResult user_name={user_name}/>
+            :user_name&&page==8?
+            <PlasticCal/>
+            :
+            null
+            }
+            {result&&page<8?
+            <button onClick={()=>{setPage(page+1)}}>다음</button>:null
+            }
+            {
+            result&&page>1?
+            <button onClick={()=>setPage(page-1)}>이전</button>:null
+            }
 
-                {!user_id?<button onClick={()=>history.replace("/test/userinfo")}>나도테스트해보기</button>:null}
+            
+
+            {!user_id?<button onClick={()=>history.replace("/test/userinfo")}>나도테스트해보기</button>:null}
 
         </div>
     )
