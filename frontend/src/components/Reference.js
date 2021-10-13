d72a62b0fe989ce7909251d87632a773cdced359
taeho <xogh8025@naver.com>
2021-10-13 19:44:19 +0900

    #style: 메인 페이지 그래프 및 테스트 버튼 디자인 구현

---
+++ frontend/src/components/Reference.js
@@ -0,0 +1,107 @@
+import React from "react";
+import styled from "styled-components";
+
+const Refer = styled.div`
+    margin-left: 10vw;
+    margin-top: 10vh;
+    text-align: left;
+    a {
+        text-decoration-line: none;
+        color: black;
+        display: block;
+        margin-top: 2px;
+        font-size: 1.2rem;
+    }
+    > div {
+        margin-top: 5vh;
+    }
+`
+
+
+export function Reference(){
+    return(
+    <Refer>
+            <div>
+                <h2>기사</h2>
+                <a href="https://www.khan.co.kr/kh_storytelling/2021/carbonprint/index.html">
+                    한겨레, "일회용 배달용기, 탄소배출 35배 더 하고 있다", 2021년 10월 1일 접속
+                </a>
+                <a href="hhttps://www.hkbs.co.kr/news/articleView.html?idxno=628288">
+                    환경일보, "지구를 덮은 플라스틱 ‘배달 어택", 2021년 10월 1일 접속
+                </a>
+                <a href="https://moneys.mt.co.kr/news/mwView.php?no=2020091816288051582">
+                    머니S뉴스, "집 밖 안나가고 배달"… 쌓이는 일회용품 어쩌나", 2021년 10월 5일 접속
+                </a>
+                <a href="http://www.gailbo.com/default/index_view_page.php?part_idx=189&idx=298440">
+                    경안일보, "코로나 이후 배달음식 소비 33.4%↑…음식물 쓰레기도 증가", 2021년 10월 5일 접속
+                </a>
+                <a href="https://moneys.mt.co.kr/news/mwView.php?no=2020091816288051582">
+                    녹색연합, "배달쓰레기 매일 830만개,배달할수록 쓰레기가 쌓이는 사회! 시민들은 강력한 대책을 촉구한다.", 2021년 10월 5일 접속
+                </a>
+                <a href="https://www.me.go.kr/home/web/board/read.do?boardMasterId=1&boardId=851780&menuId=286">
+                    환경부, "제5차 전국폐기물 통계조사…1인 하루 배출량 929.9g", 2021년 10월 5일 접속
+                </a>
+                <a href="https://www.yna.co.kr/view/AKR20190611167400009">
+                    연합뉴스, "1인당 섭취 미세플라스틱, 매주 신용카드 1장 분량", 2021년 10월 5일 접속
+                </a>
+                <a href="https://www.vegannews.co.kr/mobile/article.html?no=10928">
+                    뉴스플레닛, "넷플릭스와 배달 음식으로 보낸 주말.. 환경오염은 얼마나?", 2021년 10월 5일 접속
+                </a>
+                <a href="https://www.dongascience.com/news.php?idx=47828">
+                    뉴스플레닛, "넷플릭스와 배달 음식으로 보낸 주말.. 환경오염은 얼마나?", 2021년 10월 5일 접속
+                </a>
+                <a href="https://www.vegannews.co.kr/mobile/article.html?no=10928">
+                    동아사이언스, "넷이산화탄소 배출 줄었지만 여전한 지구온난화", 2021년 10월 6일 접속
+                </a>
+                <a href="https://home.kepco.co.kr/kepco/SM/C/D/SMCDPP00101.do?menuCd=FN2903040101">
+                    한국전력공사, "탄소 배출 계산기", 2021년 10월 6일 접속
+                </a>
+                <a href="https://www.industrynews.co.kr/news/articleView.html?idxno=43346">
+                    인더스트릿 뉴스, "온실가스 줄이지 않으면, 지구 온도 산업화 이전보다 1.5℃ 상승하고 극한 폭염 8.6배 폭증한다.", 2021년 10월 6일 접속
+                </a>
+                <a href="https://www.hankookilbo.com/News/Read/A2020111115190005789">
+                    한국일보, "끝나지 않는 '폭탄돌리기'의 역사... 쓰레기 매립지 갈등", 2021년 10월 6일 접속
+                </a>
+                <a href="http://stat.me.go.kr/nesis/mesp/info/statPolicyRecycle1.do">
+                    환경부, "수거부터 재활용까지, 궁금했던 생활폐기물 처리과정", 2021년 10월 6일 접속
+                </a>
+                <a href="http://www.greennews.co.kr/m/view.jsp?ncd=129">
+                    녹색신문, "배달음식 용기, 즉석밥 용기, 컵라면 그릇, 마스크 다 재활용 안 된다", 2021년 10월 6일 접속
+                </a>
+                <a href="https://h21.hani.co.kr/arti/society/environment/50715.html">
+                    한겨레21, "배달용기 그냥 버리나요? 씻어주세요", 2021년 10월 6일 접속   
+                </a>
+                <a href="https://www.theguardian.com/environment/ng-interactive/2020/oct/05/climate-data-dashboard-carbon-atmosphere-sea-level-arctic-ice">
+                    Guradian, "Global heating: the vital statistics from a warming world", 2021년 10월 6일 접속   
+                </a>
+                <a href="https://climatefeedback.org/claimreview/the-global-polar-bear-population-is-threatened-by-loss-of-sea-ice-contrary-to-pragerus-video-claim/">
+                    Climate Feedback, "The global polar bear population is threatened by loss of sea ice, contrary to PragerU’s video claim", 2021년 10월 6일 접속   
+                </a>
+                <a href="https://nsidc.org/arcticseaicenews/sea-ice-tools/">
+                    National Snow $ Ice center, "Sea Ice Data and Analysis Tools", 2021년 10월 6일 접속   
+                </a>
+                <a href="http://www.sisajournal.com/news/articleView.html?idxno=176598">
+                    시사저널, "인구 20만 '쓰레기 섬' GPGP", 2021년 10월 6일 접속   
+                </a>
+            </div>
+            <div>
+                <h2>논문</h2>
+                <a href="https://www.cheric.org/PDF/PIC/PC22/PC22-2-0001.pdf">
+                    '미세플라스틱 현황과 인체에 미치는 영향', 류지헌, 조충연, 공업화학 전망, 제22권 제2호, 2019, 
+                </a>
+            </div>
+            <div>
+                <h2>사이트</h2>
+                <a href="https://www.khan.co.kr/kh_storytelling/2021/carbonprint/index.html">
+                    나는 얼마나 많은 발자국을 지구에 남겼을까?,2021년 10월 6일 접속 
+                </a>
+                <a href="https://campaign.greenkorea.org/stop-disposable-items/">
+                    녹색연합, 배달1회용품 이제 그만, 2021년 10월 2일 접속 
+                </a>
+                <a href="https://developers.kakao.com/docs/latest/ko/message/js-link">
+                    카카오링크 API 문서,2021년 10월 9일 접속 
+                </a>
+            </div>
+    </Refer>
+    )
+}
