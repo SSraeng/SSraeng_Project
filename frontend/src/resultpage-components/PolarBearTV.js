1275e551dbeecfdf4eb6077e5956f38ce0785317
heejae <jhj967878@naver.com>
2021-10-13 22:17:06 +0900

    # style : 북극곰 티비 스타일 수정

--- frontend/src/resultpage-components/PolarBearTV.js
+++ frontend/src/resultpage-components/PolarBearTV.js
@@ -1,11 +1,22 @@
 import React, { useState,useEffect } from 'react';
 import ReactPlayer from 'react-player';
+import { $CombinedState } from 'redux';
 import {PolarBearImageStyle, PolarBearTVButton, PolarBearWrapper, PolarBearTVPlayer, PolarBearTVOff} from '../styled_components/PolarBearTVStyle';
 import { ResultWrapper } from '../styled_components/style';
 function PolarBearTV({content_url,content_image}) {
     
     const [tv, setTv] = useState(false);
 
+    useEffect(()=>{
+        const bodyTag = document.querySelector('body');
+        bodyTag.style.transitionProperty = 'background-color';
+        bodyTag.style.transitionDuration = '2s';
+        if(tv){
+            bodyTag.style.backgroundColor = 'rgba(0,0,0,1.0)';
+        } else{
+            bodyTag.style.backgroundColor = 'rgb(255,255,255)';
+        }
+    }, [tv])
     return (
         <div>
             <ResultWrapper>
