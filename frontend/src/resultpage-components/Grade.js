e1a42f1e1c1a80d30d87cd65011e0e54b581428b
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-10-14 14:56:44 +0900

    #UI: grade 등급에 따라 등급숫자 다르게 표현, padding top값 부여

--- frontend/src/resultpage-components/Grade.js
+++ frontend/src/resultpage-components/Grade.js
@@ -2,12 +2,12 @@ import React, { useState,useEffect } from 'react';
 import { GradeWrapper } from '../styled_components/GradeStyle';
 
 function Grade({data}) {
-
+    const color=["red","orange","yellow","#87F717","green"]
     return (
             
         data?
             <div>
-            <GradeWrapper>
+            <GradeWrapper color={color[data.tier_value-1]}>
                 <h2>{data.user_name}님, 당신은</h2>
                 <h1 >쓰랭
                     <a> {data.tier_value}</a>등급입니다.
