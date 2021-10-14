9effca1b00c5d861034a7d14ab1b880e17dc5c54
moonjiyoon <ske0830@gmail.com>
2021-10-14 23:54:09 +0900

    # style: recycletip 배경 수정

--- frontend/src/styled_components/RecycleTipStyle.js
+++ frontend/src/styled_components/RecycleTipStyle.js
@@ -9,7 +9,6 @@ export const RecycleWrapper = styled.div`
     justify-content:center; 
     text-align:center;
     padding-top:20vh;
-    background: linear-gradient(white,#C2D3DD 20% 70%, white 80%);
 
 `
 
