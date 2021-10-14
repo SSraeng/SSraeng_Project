413499557975eb5ced283e156da77618ee039334
moonjiyoon <ske0830@gmail.com>
2021-10-14 15:21:42 +0900

    # style: userInfo페이지 배달입력 버튼 수정

--- frontend/src/pages/UserInfoPage.js
+++ frontend/src/pages/UserInfoPage.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router';
 import NavBar from '../components/NavBar';
 import axios from 'axios';
 import store, { submit, user,reset, reset_result,plastic_reset, page_reset, stopwatch_reset, reset_user_id, reset_checked} from '../redux_store/store';
-import { Hover } from '../styled_components/style';
+import { Hover, ButtonStyle } from '../styled_components/style';
 import Trashes from "../components/Trashes"
 function ResetAll(history, nickname, times){
     store.dispatch(reset())
@@ -38,13 +38,12 @@ function UserInfoPage() {
                 onChange={(e) => setNickname(e.target.value)} />
             {nickname.length > 10 || nickname.length == 0?<div style={{color:"#A5BDCC", height:"5vh"}}>이름을 1자이상 10자이하로 입력하세요</div>:<div style={{height:"5vh"}}></div>}
             <h2 style={{ marginBottom:"1vh"}}>일주일 간 배달 건수</h2>
-            <button style={{borderRadius:"1vh",marginRight:"1vh",border : "0",marginBottom:"6vh", width:"4vh", backgroundColor:"#A5BDCC"}} 
-            onClick={times > 0 ? () => setTimes(times - 1) : null}>-</button>
+            <ButtonStyle onClick={times > 0 ? () => setTimes(times - 1) : null}>-</ButtonStyle>
 
             <input value={times} style={{borderRadius:"10px", border: "1px solid #A5BDCC",  textAlign:"center", width:"15rem", height:"2rem" }} 
             onChange={(e) => setTimes(parseInt(e.target.value))} readOnly/>
 
-            <button style={{borderRadius:"1vh",marginLeft:"1vh",border : "0",marginBottom:"6vh", width:"4vh", backgroundColor:"#A5BDCC"}} onClick={() => setTimes(parseInt(times + 1))}>+</button>
+            <ButtonStyle onClick={() => setTimes(parseInt(times + 1))}>+</ButtonStyle>
             <p></p>
             <Hover>
             <button style={{textAlign:"center", 
