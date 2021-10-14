d4626a50bc98fa5413e855e539c1a88bf599c756
taeho <xogh8025@naver.com>
2021-10-14 18:46:29 +0900

    #style: loadingPage 미디어 쿼리 적용

--- frontend/src/pages/LoadingPage.js
+++ frontend/src/pages/LoadingPage.js
@@ -1,34 +1,118 @@
 import { useEffect, React } from 'react';
 import { useHistory } from 'react-router';
 import { useLocation } from 'react-router';
+import { Wave } from '../components/Wave';
+import styled, { keyframes } from 'styled-components';
+
+const LoadingTitle = styled.p`
+    color: #6EB3D0;
+    font-size: 3.2rem;
+    font-weight: 900;
+    margin-bottom: 1vh;
+    text-shadow: 1px 2px 3px black;
+
+    @media only screen and (max-width:768px){
+        font-size: 1.5rem;
+    };
+`
+const loader = keyframes`
+    0% {
+        width: 0px;
+    }
+    70% {
+        width: 100%;
+        opacity: 1;
+    }
+    90% {
+        opacity: 0;
+        width: 100%;
+    }
+    100% {
+        opacity: 0;
+        width: 0px;
+    }
+`
+
+const Loader = styled.div`
+    margin: 0 auto;
+    position: relative;
+    width: 35vw;
+    height: 40px;
+    
+    :before {
+        content: "";
+        position: absolute;
+        background-color: #6EB3D0;
+        border-radius: 10px;
+        top: 0px;
+        left: 0px;
+        height: 30px;
+        width: 0px;
+        z-index: 0;
+        opacity: 1;
+        animation: ${loader} 6s ease-in-out infinite;
+    }
+
+    :after {
+        content: "LOADING ...";
+        position: absolute;
+        color: #fff;
+        font-family: "Elice_Bold";
+        font-size: 20px;
+        width: 100%;
+        height: 20px;
+        line-height: 30px;
+        left: 0;
+        top: 0;
+    };
+
+    @media only screen and (max-width:768px){
+        width: 60vw;
+        height: 20px;
+        :before {
+            height: 20px;
+        }
+        :after {
+            font-size: 10px;
+            height: 12px;
+            line-height: 20px;
+        }
+    };
+
+`
+const Polarbear = styled.img`
+    width: 30vw;
+    overflow: hidden;
+    border-radius: 50%;
+    
+    @media only screen and (max-width:768px){
+        width: 50vw;
+    };
+`
 
 function LoadingPage(){
     const location= useLocation();
-    const next = location.state.next;
-    const history = useHistory();
-    useEffect(()=>{
-        const toResultPage= () => {
-            setTimeout(()=>{
-                history.push(next)
-            }, 5000)
-        }
-        toResultPage()
-    },[])
+    // const next = location.state.next;
+    // const history = useHistory();
+    // useEffect(()=>{
+    //     const toResultPage= () => {
+    //         setTimeout(()=>{
+    //             history.push(next)
+    //         }, 5000)
+    //     }
+    //     toResultPage()
+    // },[])
 
     return(
         <div className="loadingPage">
             <div className="loading_header">
-                <p   className="loading_header_title">로딩 중...</p>
-                <div className="loading_header_box">
-                    <div className="loader"></div>
-                </div>
+                <LoadingTitle>로딩 중...</LoadingTitle>
+                <Loader></Loader>
             </div>
-            <div className="loading_content">
-                <div className="loading_content_image_box" >
-                    <img className="loading_content_image_bear" src="images/polarbear.png" alt="빙하 위의 위태로운 북극곰"/>
-                    <img className="loading_content_image_sea" src="images/sea.png" alt="바다 일러스트레이션"/>
-                </div>
+            <div className="loading_image_box">
+                    <Polarbear src="images/polarbear.png" alt="빙하 위의 위태로운 북극곰"/>
             </div>
+            <Wave/>
         </div>
     )
 }
