f6097a2240e6d2bc85b75db7c1a4a1900f62c7ef
taeho <xogh8025@naver.com>
2021-10-15 13:23:44 +0900

    #style: 어바웃 페이지 내용 추가 및 모바일 뷰 구현

--- frontend/src/etc/memberList.js
+++ frontend/src/etc/memberList.js
@@ -2,49 +2,49 @@ export const memberList = [
     {
         id:1,
         name:"정희재",
-        email: "abc@gmail.com",
-        content : "자기 소개 칸을 채워주세요",
+        email: "gmlwo0616@gmail.com",
+        content : "클린하고 퓨어한 개발자 정희재입니다^^",
         position : "백엔드/ 데이터분석",
-        ssRank : "1등급"
+        ssRank : "5등급"
     },
     {
         id:2,
         name:"서명현",
-        email: "abc@gmail.com",
-        content : "자기 소개 칸을 채워주세요",
+        email: "mh93@kakao.com",
+        content : "100% ISFP",
         position : "PM/ 데이터분석",
-        ssRank : "2등급"
+        ssRank : "1등급"
     },
     {
         id:3,
         name:"백승욱",
-        email: "abc@gmail.com",
-        content : "자기 소개 칸을 채워주세요",
+        email: "tbvjqkdlcm1@naver.com",
+        content : "해위용~",
         position : "백엔드",
         ssRank : "3등급"
     },
     {
         id:4,
         name:"문지윤",
-        email: "abc@gmail.com",
-        content : "자기 소개 칸을 채워주세요",
+        email: "ske0830@gmail.com",
+        content : "I'm on the Next Level~",
         position : "백엔드/ 데이터분석",
         ssRank : "4등급"
     },
     {
         id:5,
         name:"남궁은선",
-        email: "abc@gmail.com",
-        content : "자기 소개 칸을 채워주세요",
+        email: "nammugmuhg@gmail.com",
+        content : "극곰이 귀여엉",
         position : "프론트엔드",
         ssRank : "5등급"
     },
     {
         id:6,
         name:"김태호",
-        email: "abc@gmail.com",
-        content : "자기 소개 칸을 채워주세요",
+        email: "xogh8025@naver.com",
+        content : "열정 열정 열정!",
         position : "프론트엔드/ 데이터분석",
-        ssRank : "6등급"
+        ssRank : "4등급"
     },
 ]
